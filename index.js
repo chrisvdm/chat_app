448612fa5ef7897782390858add66d63dfa80503
3leanore <nymanchristine@gmail.com>
2016-08-24 11:35:47 +0200

    Added external stylesheet properly

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+var fs = require('fs');
 var http = require('http').Server(app);
+
+// Use this to serve static files like styling
+app.use('/static', express.static('static'));
 
 //We define a route handler / that gets called when we hit our website home.
 app.get('/', function(req, res){
   // frontend
-  res.sendFile('index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 // Server port
